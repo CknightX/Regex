9823f68c927eaf289374f599b56c9ef0297dfb8a
CknightX <caokeck123@163.com>
2017-04-08 20:04:45 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<memory>
+#include "test.h"
+using namespace std;
+
+
+int main()
+{
+	
+	//Parser a("(ab.+)*|a*");
+	//auto b = a.GenTree();
+	A a;
+	cin.get();
+
+}
